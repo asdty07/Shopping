5448b00064d7884d04bb14140bed75316c0213e9
Github <asdty07@gmail.com>
2024-02-17 11:54:41 +0900

    test

---
+++ nomargin.com/nomargin/src/main/java/EZ/nomargin/repository/ItemRepository.java
@@ -0,0 +1,19 @@
+package EZ.nomargin.repository;
+
+import EZ.nomargin.domain.item.Item;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemRepository {
+    Item save(Item item);
+    Item findById(Long itemId);
+    List<Item> findAll();
+    void update(Long itemId, Item updateItem);
+    void delete(Long itemId);
+    List<Item> findTop();
+    List<Item> findBottom();
+    List<Item> findOuter();
+
+
+}
